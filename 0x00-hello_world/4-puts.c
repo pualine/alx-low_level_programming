60efe1c1f7b8b76e74734574d25f1eade9397c1b
pualine <prisypauline@gmail.com>
2023-03-22 09:29:47 -0700

    Update Hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-puts("%d Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+
+	return (0);
+}
