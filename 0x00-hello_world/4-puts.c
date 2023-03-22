748527cb80ff63fecda7a26b378fc743f3c83647
pualine <prisypauline@gmail.com>
2023-03-22 09:49:07 -0700

    Update Hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
+        puts("\"Programming is like building a multilingual puzzle");
 
 
         return (0);
+
 }
