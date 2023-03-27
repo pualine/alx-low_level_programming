b1b0fa664b817f12f1f3b173a0a2ced17bdd5d92
pualine <prisypauline@gmail.com>
2023-03-27 14:17:27 -0700

    Update my putchar program

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,19 @@
   * Return: ...
   *
   */
-int jack_bauer(void)
-{
-   int a, b;
-
-   for (a = 0; a <= 23; a++)
-   {
-      for (b = 0; b <= 59; b++)
-	  {
-	     _putchar (a / 10 + '0');
-		 _putchar (a % 10 + '0');
-		 _putchar (':');
-		 _putchar (b / 10 + '0');
-		 _putchar (b % 10 + '0');
-		 _putchar ('\n');
-	  }
-   }
+void jack_bauer(void)
+{ 
+	int a, b;
+	for (a = 0; a <= 23; a++)
+	{ 
+		for (b = 0; b <= 59; b++)
+		{ 
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+		} 
+	}
 }
--- 0x02-functions_nested_loops/9-main.h
+++
@@ -1,6 +0,0 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-void times_table(void);
